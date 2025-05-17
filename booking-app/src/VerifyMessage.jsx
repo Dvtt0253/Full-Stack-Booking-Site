8cee3cc8f404c14614ccef467ee542f3dc10635d
Dvtt0253 <Dvtt0253@gmail.com>
2025-05-17 07:49:51 -0500

    added jsx component

---
+++ booking-app/src/VerifyMessage.jsx
@@ -0,0 +1,12 @@
+function VerifyMessage(){
+
+    return(
+        <div>
+            <h2>Verify Your Email Address</h2>
+            <p>You will recieve an email shortly to verify your email address. Pleae verify to proceed.</p>
+        </div>
+    );
+
+
+}
+export default VerifyMessage
